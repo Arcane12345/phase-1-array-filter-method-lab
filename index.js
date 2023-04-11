2df96b969857b4e6035348d4cc836e7a6b93ff42
Eric <macario41@gmail.com>
2023-04-11 17:48:35 -0400

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    const newList = drivers.filter(function (turtle) {
+        return turtle.toLowerCase() === string.toLowerCase()
+    })
+    return newList
+}
+
+function fuzzyMatch(drivers, string) {
+    const driversThatMatch = drivers.filter(function (driver) {
+        return driver.startsWith(string)
+    });
+    return driversThatMatch
+}
+
+function matchName(drivers, string) {
+    const exactMatch = drivers.filter((drivers) => drivers.name === string)
+    return exactMatch
+}
